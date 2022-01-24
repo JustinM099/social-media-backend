076161b106531923586860db668632c05a6ec143
JustinM099 <justinm099@mac.com>
2022-01-23 22:40:00 -0800

    building route structure

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, 'A valid email address is required']
+      match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, "I'd like you to enter a valid email, please."]
     },
     thoughts: [{
       type: Schema.Types.ObjectId,
